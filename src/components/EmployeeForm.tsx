f7bf067fbc7b7e396e611f8d4d7e87243457be37
jayagajjar <jaya.wired@gmail.com>
2025-03-15 14:49:40 +1100

    add employee

--- src/components/EmployeeForm.tsx
+++ src/components/EmployeeForm.tsx
@@ -1,13 +1,43 @@
 import React, { useState } from "react";
+import { Employee } from "../types";
+import { getFromLocalStorage, saveToLocalStorage } from "../utils/utils";
+import { StorageKeys } from "../utils/stogareKeys";
 
-const EmployeeForm = ({ customerId }) => {
+const EmployeeForm = ({ customerId, refresh }) => {
   const [name, setName] = useState("");
   const [role, setRole] = useState("");
   const [email, setEmail] = useState("");
 
-  console.log("customerId ", customerId);
+  const handleAddEmployee = (e: React.FormEvent) => {
+    e.preventDefault();
 
-  const handleAddEmployee = () => {};
+    if (!name || !role || !email.includes("@")) {
+      alert("All fields are required");
+      return;
+    }
+
+    const newEmployee: Employee = {
+      id: Date.now().toString(),
+      name,
+      role,
+      email,
+    };
+
+    const customers = getFromLocalStorage(StorageKeys.CUSTOMERS) || [];
+    const customerIndex = customers.findIndex((c) => c.id === customerId);
+
+    if (customerIndex !== -1) {
+      customers[customerIndex].employees.push(newEmployee);
+      saveToLocalStorage(StorageKeys.CUSTOMERS, customers);
+      saveToLocalStorage(StorageKeys.LOGGED_IN_USER, customers[customerIndex]);
+      console.log("Employee Added");
+      refresh();
+    }
+
+    setName("");
+    setRole("");
+    setEmail("");
+  };
 
   return (
     <div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { getFromLocalStorage, saveToLocalStorage } from "../utils/utils";
+import React, { useState } from "react";
+import { getFromLocalStorage } from "../utils/utils";
 import EmployeeForm from "../components/EmployeeForm";
 import EmployeeList from "../components/EmployeeList";
 import { useNavigate } from "react-router-dom";
@@ -7,7 +7,15 @@ import { StorageKeys } from "../utils/stogareKeys";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const customer = getFromLocalStorage(StorageKeys.LOGGED_IN_USER);
+  const [customer, setCustomer] = useState(
+    getFromLocalStorage(StorageKeys.LOGGED_IN_USER)
+  );
+
+  const refreshEmployees = () => {
+    const updatedCustomer =
+      getFromLocalStorage(StorageKeys.LOGGED_IN_USER) || [];
+    setCustomer(updatedCustomer);
+  };
 
   const handleSignOut = () => {
     localStorage.removeItem(StorageKeys.LOGGED_IN_USER);
@@ -18,7 +26,7 @@ const Dashboard = () => {
       <h2>Welcome, {customer?.fullName}</h2>
       {customer && (
         <>
-          <EmployeeForm customerId={customer.id} />
+          <EmployeeForm customerId={customer.id} refresh={refreshEmployees} />
           <EmployeeList employees={customer.employees} />
           <button type="submit" onClick={handleSignOut}>
             Sign Out
